a5b2bc74e6649e0b7e254497614bef5ac6e406b4
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-07-11 10:07:46 +0200

    movement animation added to character

--- models/character.class.js
+++ models/character.class.js
@@ -2,9 +2,36 @@ class Character extends MovableObject {
     height = 260;
     width = 120;
     y = 170;
+    IMAGES_WALKING = [
+        'graphics/2_character_pepe/2_walk/W-21.png',
+        'graphics/2_character_pepe/2_walk/W-22.png',
+        'graphics/2_character_pepe/2_walk/W-23.png',
+        'graphics/2_character_pepe/2_walk/W-24.png',
+        'graphics/2_character_pepe/2_walk/W-25.png',
+        'graphics/2_character_pepe/2_walk/W-26.png',
+    ];
+
+    currentImage = 0;
     
     constructor() {
         super().loadImage('graphics/2_character_pepe/2_walk/W-21.png'); 
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    /**
+     * % modulo = mathematischer Rest
+     * 
+     */
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
+       
     }
     
     jump(){
--- models/clouds.class.js
+++ models/clouds.class.js
@@ -5,5 +5,15 @@ class Clouds extends MovableObject {
 
     constructor() {
         super().loadImage('graphics/5_background/layers/4_clouds/1.png');
+
+        this.x = Math.random() * 500;
+        this.animate();
+
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
     }
 }
