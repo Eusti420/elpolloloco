75e91a09c0b68d4f949edcbd067f179f0b999d15
Eusti420 <163431211+Eusti420@users.noreply.github.com>
2024-07-11 15:29:48 +0200

    movement left and right added

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
     height = 260;
     width = 120;
     y = 170;
+    speed = 10;
     IMAGES_WALKING = [
         'graphics/2_character_pepe/2_walk/W-21.png',
         'graphics/2_character_pepe/2_walk/W-22.png',
@@ -24,6 +25,16 @@ class Character extends MovableObject {
      */
     animate() {
         setInterval(() => {
+            if(this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+
+            if(this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60)
+
+        setInterval(() => {
 
             if(this.world.keyboard.RIGHT) {
                 let i = this.currentImage % this.IMAGES_WALKING.length;
@@ -31,7 +42,7 @@ class Character extends MovableObject {
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100);
+        }, 50);
        
     }
     
